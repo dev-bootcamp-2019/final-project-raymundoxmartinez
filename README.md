51452242887898c1dc2f31fd686ff34756984bd8
raymundoxmartinez <fullstackray@gmail.com>
2019-01-27 18:41:33 -0800

    added changes

---
+++ README.md
@@ -0,0 +1,23 @@
+This project is a simple market place that allows users to add items that are for sale to their store.
+
+It also allows users to buy items.
+
+To start the app on a local server follow the following steps:
+1. ganache-cli
+2. truffle compile
+3. truffle migrate
+4. add mnemonic to metamask
+5. navigate to /client
+6. npm i
+7. npm start
+8. on your browser you will see the login page
+9. click enter
+10. click on the cross to begin adding items
+    ***make sure the image you are adding is small about 6kbs***
+    ***the smaller the better***
+11. you will be prompted by metamask to accept the transaction
+12. you can then test the buy functionality by clicking on the buy button
+13. you will then again be prompted by metamask to accept the transaction
+14. the bought item's button has been disabled
+
+That's all. Thanks!
--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -8,23 +8,11 @@ pragma solidity ^0.5.0;
 
 contract SupplyChain {
 
-  /* set owner */
     address payable owner;
-
-  /* Add a variable called skuCount to track the most recent sku # */
+    bool public contractPaused = false;
     uint public skuCount;
-  /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
-     Call this mappings items
-  */
     mapping (uint => Item ) public items;
 
-  /* Add a line that creates an enum called State. This should have 4 states
-    ForSale
-    Sold
-    Shipped
-    Received
-    (declaring them in this order is important for testing)
-  */
   enum State {
     ForSale,
     Sold,
@@ -32,11 +20,6 @@ contract SupplyChain {
     Received
   }
 
-  /* Create a struct named Item.
-    Here, add a name, sku, price, state, seller, and buyer
-    We've left you to figure out what the appropriate types are,
-    if you need help you can ask around :)
-  */
     struct Item {
       string name;
       uint sku;
@@ -46,15 +29,12 @@ contract SupplyChain {
       address payable seller;
       address payable buyer;
     }
-  /* Create 4 events with the same name as each possible State (see above)
-    Each event should accept one argument, the sku*/
 
     event ForSale(uint indexed sku);
     event Sold(uint indexed sku);
     event Shipped(uint indexed sku);
     event Received(uint indexed sku);
 
-/* Create a modifer that checks if the msg.sender is the owner of the contract */
 
   modifier isOwner () { require(msg.sender == owner); _;}
 
@@ -69,9 +49,6 @@ contract SupplyChain {
     items[_sku].buyer.transfer(amountToRefund);
   }
 
-  /* For each of the following modifiers, use what you learned about modifiers
-   to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. */
   modifier forSale(uint _sku){
     require(items[_sku].state == State.ForSale, "Item is not For Sale");
     _;
@@ -89,6 +66,14 @@ contract SupplyChain {
     _;
   }
 
+//Here is a circuit breaker to that prevents all 
+//app functionality if something bad occurs
+//if the contract is paused, stop the modified function
+  modifier checkIfPaused() {
+    require(contractPaused == false);
+    _;
+  }
+
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
@@ -101,12 +86,6 @@ contract SupplyChain {
     items[skuCount] = Item({name: _name, sku:skuCount, price: _price, image: _image, state: State.ForSale, seller: msg.sender, buyer: address(0)});
     skuCount = skuCount + 1;
   }
-
-  /* Add a keyword so the function can be paid. This function should transfer money
-    to the seller, set the buyer as the person who called this transaction, and set the state
-    to Sold. Be careful, this function should use 3 modifiers to check if the item is for sale,
-    if the buyer paid enough, and check the value after the function is called to make sure the buyer is
-    refunded any excess ether sent. Remember to call the event associated with this function!*/
 
   function buyItem(uint sku)
     public
@@ -121,8 +100,11 @@ contract SupplyChain {
     emit Sold(sku);
   }
 
-  /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
-  is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
+//only an owner can call 
+  function circuitBreaker() public isOwner {
+    if(contractPaused == false) { contractPaused = true; }
+    else { contractPaused = false; }
+  }
   function shipItem(uint sku)
     public
     sold(sku)
@@ -132,8 +114,6 @@ contract SupplyChain {
     emit Shipped(sku);
   }
 
-  /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
-  is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
     public
     shipped(sku)
@@ -143,7 +123,6 @@ contract SupplyChain {
     emit Received(sku);
   }
 
-  /* We have these functions completed so we can run tests, just ignore it :) */
   function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer, string memory image) {
     name = items[_sku].name;
     sku = items[_sku].sku;
